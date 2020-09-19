57d07b320eaa518684023aa9cf148efdfaf0636d
Samir Patel <samirpatel@utexas.edu>
2020-09-19 12:32:13 -0500

    Find the Town Judge

---
+++ identifySink.java
@@ -0,0 +1,21 @@
+class identifySink {
+    public int findJudge(int N, int[][] trust) {
+        if (trust.length == 0 && N == 1)
+            return 1;
+
+
+        int[] trustValues = new int[N + 1];
+
+        for (int i = 0; i < trust.length; i++){
+            trustValues[trust[i][0]]--;
+            trustValues[trust[i][1]]++;
+        }
+
+        for (int i = 0; i < trustValues.length; i++){
+            if (trustValues[i] == N - 1) return i;
+        }
+
+        return -1;
+
+    }
+}
